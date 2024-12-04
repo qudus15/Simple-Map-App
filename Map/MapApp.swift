3ab1504aaf0e031c61158c02687d29f3774692eb
qudus15 <meet.qudus@gmail.com>
2024-12-03 19:01:30 -0500

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Red Whale inc. on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapTests/MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTests.swift
+//  MapTests
+//
+//  Created by Red Whale inc. on 03/12/2024.
+//
+
+import Testing
+@testable import Map
+
+struct MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
